0fa37fa090856b31ce1ff6ac09954a9ce5514498
Ece <Ece@DESKTOP>
2021-11-28 15:48:23 +0300

    Initial commit

---
+++ src/main/java/payroll/ApplicationConfiguration.java
@@ -0,0 +1,19 @@
+package payroll;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ApplicationConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        httpSecurity.cors().and().csrf().disable();
+
+    }
+
+
+
+}
