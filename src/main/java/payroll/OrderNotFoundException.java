134dbab0abf5e1ef620429ad8a7dff5f3a60c13d
Ece <Ece@DESKTOP>
2021-11-29 12:40:50 +0300

    added orders

---
+++ src/main/java/payroll/OrderNotFoundException.java
@@ -0,0 +1,8 @@
+package payroll;
+
+public class OrderNotFoundException extends RuntimeException {
+
+    public OrderNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
